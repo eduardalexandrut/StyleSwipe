a274dc1185a18e4e9a4d094ccb640284441b8b94
eduardalexandrut <aeduardtoni@gmail.com>
2024-01-06 16:21:01 +0100

    Created Pin.js file containing the Pin classs. Added this file in home.html.

---
+++ js/Pin.js
@@ -0,0 +1,24 @@
+//Pin class.
+class Pin {
+    x;
+    y;
+    strokeStyle = "white";
+    fillStyle = "#9013FE";
+    lineWidth =6;
+    radius = 8;
+    
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    };
+    
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0 , 2*Math.PI);
+        ctx.strokeStyle=this.strokeStyle;
+        ctx.fillStyle = this.fillStyle;
+        ctx.lineWidth = this.lineWidth;
+        ctx.stroke();
+        ctx.fill();
+    }
+};
