f7b46ceabb2838bd0a3378bcf2115e87d21aa23c
eduardalexandrut <aeduardtoni@gmail.com>
2024-01-06 15:35:10 +0100

    Created classses.js file, which will contain all the .j classes. Created item class.

---
+++ js/classes.js
@@ -0,0 +1,41 @@
+//Item class.
+class Item {
+    constructor(name, brand, link, price, size, x, y) {
+        this.name = name;
+        this.brand = brand;
+        this.link = link;
+        this.price = price;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    getName() {
+        return this.name;
+    }
+
+    getBrand() {
+        return this.brand;
+    }
+
+    getLink() {
+        return this.link;
+    }
+
+    getPrice() {
+        return this.price;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
+    }
+}
