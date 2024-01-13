648b725a27c459b66ee9216360eac443b0ee28d6
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2024-01-13 15:37:29 +0100

    Changed db's table structures.

--- db/create.sql
+++ db/create.sql
@@ -1,79 +1,86 @@
-/*User's table*/
 CREATE TABLE `User` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(50) PRIMARY KEY,
     `name` VARCHAR(50) NOT NULL,
     `surname` VARCHAR(50) NOT NULL,
-    `username` VARCHAR(50) UNIQUE NOT NULL,
-    `password` VARCHAR(100) NOT NULL, -- Note: Passwords should be hashed for security
+    `password` VARCHAR(255) NOT NULL, 
     `date_of_birth` DATE NOT NULL,
-    `gender` ENUM('Male', 'Female', 'Neutral')
+    `gender` ENUM('Male', 'Female', 'Neutral'),
+    `profile_image` VARCHAR(255) NOT NULL 
 );
-
-/*Post Table*/
+/* Post Table */
 CREATE TABLE `Post` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
+    `user_username` VARCHAR(50) NOT NULL,
     `image` VARCHAR(100) NOT NULL,
     `posted` DATETIME NOT NULL,
     `comment` TEXT,
-    FOREIGN KEY (`user_id`) REFERENCES `User`(`id`)
+    FOREIGN KEY (`user_username`) REFERENCES `User`(`username`)
 );
 
-/*Item Table*/
+/* Item Table */
 CREATE TABLE `Item` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `post_id` INT,
     `name` VARCHAR(100) NOT NULL,
     `brand` VARCHAR(100) NOT NULL,
-    `link` VARCHAR(255), -- Assuming a URL link for the item
+    `link` VARCHAR(255),
     `size` VARCHAR(50) NOT NULL,
-    `price` FLOAT(6, 2), -- Assuming a decimal data type for price
+    `price` FLOAT(6, 2),
     `currency` VARCHAR(10),
-    `x` FLOAT NOT NULL, -- Assuming numeric coordinates for x and y
+    `x` FLOAT NOT NULL,
     `y` FLOAT NOT NULL,
     FOREIGN KEY (`post_id`) REFERENCES `Post`(`id`)
 );
 
-/*Like Table*/
+/* Like Table */
 CREATE TABLE `Like` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
+    `user_username` VARCHAR(50) NOT NULL,
     `post_id` INT NOT NULL,
     `date_liked` DATETIME NOT NULL,
-    FOREIGN KEY (`user_id`) REFERENCES `User`(`id`),
-    FOREIGN KEY (`post_id`) REFERENCES `Post`(`id`)
-);
-/*Comment Table*/
-CREATE TABLE `Comment` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
-    `post_id` INT NOT NULL,
-    `comment_text` TEXT NOT NULL,
-    `date_posted` DATETIME NOT NULL, 
-    FOREIGN KEY (`user_id`) REFERENCES `User`(`id`),
-    FOREIGN KEY (`post_id`) REFERENCES `Post`(`id`)
-);
-/*Star Table*/
-CREATE TABLE `Star` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
-    `post_id` INT NOT NULL,
-    `date_starred` DATETIME NOT NULL,
-    FOREIGN KEY (`user_id`) REFERENCES `User`(`id`),
+    FOREIGN KEY (`user_username`) REFERENCES `User`(`username`),
     FOREIGN KEY (`post_id`) REFERENCES `Post`(`id`)
 );
 
-/* Notifications. */
+/* Comment Table */
+CREATE TABLE `Comment` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `user_username` VARCHAR(50) NOT NULL,
+    `post_id` INT NOT NULL,
+    `comment_text` TEXT NOT NULL,
+    `date_posted` DATETIME NOT NULL,
+    FOREIGN KEY (`user_username`) REFERENCES `User`(`username`),
+    FOREIGN KEY (`post_id`) REFERENCES `Post`(`id`)
+);
+
+/* Star Table */
+CREATE TABLE `Star` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `user_username` VARCHAR(50) NOT NULL,
+    `post_id` INT NOT NULL,
+    `date_starred` DATETIME NOT NULL,
+    FOREIGN KEY (`user_username`) REFERENCES `User`(`username`),
+    FOREIGN KEY (`post_id`) REFERENCES `Post`(`id`)
+);
+
+/* Notifications Table */
 CREATE TABLE `Notification` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `notification_type` ENUM('liked', 'starred', 'commented'),
-    `from_user_id` INT NOT NULL,
-    `to_user_id` INT NOT NULL,
+    `from_user_username` VARCHAR(50) NOT NULL,
+    `to_user_username` VARCHAR(50) NOT NULL,
     `post_id` INT NOT NULL,
-    `seen` BOOLEAN DEFAULT 0 NOT NULL,  -- Modificato il valore predefinito
+    `seen` BOOLEAN DEFAULT 0 NOT NULL,
     `date_posted` DATETIME NOT NULL,
-    FOREIGN KEY (`from_user_id`) REFERENCES `User`(`id`),
-    FOREIGN KEY (`to_user_id`) REFERENCES `User`(`id`),
+    FOREIGN KEY (`from_user_username`) REFERENCES `User`(`username`),
+    FOREIGN KEY (`to_user_username`) REFERENCES `User`(`username`),
     FOREIGN KEY (`post_id`) REFERENCES `Post`(`id`)
 );
 
+CREATE TABLE `Follow` (
+    `follower_username` VARCHAR(50),
+    `following_username` VARCHAR(50),
+    PRIMARY KEY (`follower_username`, `following_username`),
+    FOREIGN KEY (`follower_username`) REFERENCES `User`(`username`),
+    FOREIGN KEY (`following_username`) REFERENCES `User`(`username`)
+);
