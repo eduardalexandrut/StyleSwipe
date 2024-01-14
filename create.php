1713c8650ccfef3eafb392c2d05d1610b120c8d5
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2024-01-14 17:05:11 +0100

    Added php to display loged user's username in create-view.php.

--- create.php
+++ create.php
@@ -4,6 +4,8 @@ require_once 'bootstrap.php';
 $templateParams["title"] = "New Outfit";
 $templateParams["name"] = "create-view.php";
 $templateParams["profilepic"] = $_SESSION["profilepic"];
+$templateParams["username"] = $_SESSION["username"];
+
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     // Check if comment and items are set in $_POST
