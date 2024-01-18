976e6ca3eaae0590983be9cc79136f26f90ea9cb
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2024-01-18 15:35:00 +0100

    Eliminated Item.js class from single files.

--- template/create-view.php
+++ template/create-view.php
@@ -91,5 +91,7 @@
 
 <script src="js/open-search.js"></script>             
 <script  src="js/create-view.js"></script>
+<script  src="js/Item.js"></script>
+<script  src="js/Pin.js"></script>
 <script  src="js/notifications.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
